0880cc017641183d0444ca05c7b0cb992b360989
zebesta <chris.sebesta@gmail.com>
2016-07-29 14:20:52 -0400

    experiments for building and saving JSON, adding elements based on user input, and more

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
-
 var bodyParser = require('body-parser');
-var multer  = require('multer');
+var fs = require('fs');
+var jsonfile = require('jsonfile')
+var file = 'data.json'
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: './tmp/'}).single('photo'));
 
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
 
-app.post('/file_upload', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
 
-   console.log(req.files.file.name);
-   console.log(req.files.file.path);
-   console.log(req.files.file.type);
-
-   var file = __dirname + "/" + req.files.file.name;
-   fs.readFile( req.files.file.path, function (err, data) {
-        fs.writeFile(file, data, function (err) {
-         if( err ){
-              console.log( err );
-         }else{
-               response = {
-                   message:'File uploaded successfully',
-                   filename:req.files.file.name
-              };
-          }
-          console.log( response );
-          res.end( JSON.stringify( response ) );
-       });
-   });
+   // Prepare output in JSON format
+   response = {
+       position:1,
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
+   };
+   var listOfNames = [response, response, response]
+   listOfNames.push(response);
+   console.log(response);
+  jsonfile.writeFile(file, listOfNames, {spaces: 2}, function(err) {
+    console.error(err);
+  })
+   res.end(JSON.stringify(listOfNames));
 })
 
 var server = app.listen(8081, function () {
